a89f44605a9121fa2d1c904b7311e3bfe56adb07
Ruben Hontalvilla <rhontalvillam02@educantabria.es>
2021-11-23 20:36:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ataulfo/FP.java
@@ -0,0 +1,14 @@
+package com.example.ataulfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fp);
+    }
+}
